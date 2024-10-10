8e840254d968c6c4d66624c1f1531d5fc21f1941
noman <noman@vfairs.com>
2024-10-10 12:38:33 +0500

    remove comments

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AuthModule } from '../auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from '../user/user.module';
 
-// uri = "mongodb+srv://nomankabeerr:UBhAFlF38u8jthwB@nomankabeer.48lqc.mongodb.net/nest_angular?retryWrites=true&w=majority&appName=nomankabeer";
-// uri = 'mongodb+srv://nomankabeerr:UBhAFlF38u8jthwB@nomankabeer.48lqc.mongodb.net/nest_angular?retryWrites=true&w=majority';
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_DB_URI, {
